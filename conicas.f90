dcb14422a1bed782eafee9479040b63f17a2192b
timotheosf <timoteofassoni@gmail.com>
2024-11-15 08:37:56 -0300

    Add comentários

--- conicas.f90
+++ conicas.f90
@@ -14,49 +14,58 @@ private x_r , y_r , num_hits , num_trials , seed , generator
 contains
 
 subroutine elipse( a , b , L_x , L_y , area )
+    !   Subrotina que pega os dados passados para o programa principal, 
+    !   gera as informações geométricas da ELIPSE 
+    !   e aplica o método de Monte Carlo para sua medida
     real(kind=dp), intent(in) :: a , b , L_x , L_y
     real(kind=dp), intent(out) :: area
     call generator%init(seed)
     do i = 1 , num_trials
-        x_r = generator%real( -0.5*L_x , 0.5*L_x )
+        x_r = generator%real( -0.5*L_x , 0.5*L_x ) ! Gera duplas de números ao longo de todo o sistema
         y_r = generator%real( -0.5*L_y , 0.5*L_y )
-        if ( -b*sqrt(1. - (x_r/a)**2)<= y_r  .and. y_r <= b*sqrt(1. - (x_r/a)**2) ) then
+        if ( -b*sqrt(1. - (x_r/a)**2)<= y_r  .and. y_r <= b*sqrt(1. - (x_r/a)**2) ) then ! Condição para o número ser interno à elipse
             num_hits = num_hits + 1
         endif
     enddo
-    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y
+    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y ! Medida da elipse
 end subroutine elipse
 
 subroutine hiperbole( a , b , L_x , L_y , area  ) 
+    !   Subrotina que pega os dados passados para o programa principal, 
+    !   gera as informações geométricas da HIPÉRBOLE 
+    !   e aplica o método de Monte Carlo para sua medida
     real(kind=dp), intent(in) :: a , b , L_x , L_y
     real(kind=dp), intent(out) :: area
     call generator%init(seed)
     do i = 1 , num_trials
-        x_r = generator%real( -0.5*L_x , 0.5*L_x )
+        x_r = generator%real( -0.5*L_x , 0.5*L_x ) ! Gera duplas de números ao longo de todo o sistema
         y_r = generator%real( -0.5*L_y , 0.5*L_y )
         if ( x_r <= a .or. a <= x_r ) then
-            if ( -b*sqrt((x_r/a)**2 - 1.) <= y_r .and. y_r <= b*sqrt((x_r/a)**2 - 1.) ) then
+            if ( -b*sqrt((x_r/a)**2 - 1.) <= y_r .and. y_r <= b*sqrt((x_r/a)**2 - 1.) ) then ! Condição para o número ser interno à hipérbole
                 num_hits = num_hits + 1
             endif
         endif
     enddo
-    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y
+    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y ! Medida da hipérbole
 end subroutine hiperbole
 
 subroutine parabola( a , L_x , L_y , area )
+    !   Subrotina que pega os dados passados para o programa principal, 
+    !   gera as informações geométricas da PARÁBOLA 
+    !   e aplica o método de Monte Carlo para sua medida
     real(kind=dp), intent(in) :: a , L_x , L_y
     real(kind=dp), intent(out) :: area
     call generator%init(seed)
     do i = 1 , num_trials
-        x_r = generator%real( -0.5*L_x , 0.5*L_x )
+        x_r = generator%real( -0.5*L_x , 0.5*L_x ) ! Gera duplas de números ao longo de todo o sistema
         y_r = generator%real( -0.5*L_y , 0.5*L_y )
         if ( 0. <= x_r ) then
-            if ( -2.*sqrt(a*x_r) <= y_r .and. y_r <= 2.*sqrt(a*x_r) ) then
+            if ( -2.*sqrt(a*x_r) <= y_r .and. y_r <= 2.*sqrt(a*x_r) ) then ! Condição para o número ser interno à parábola
                 num_hits = num_hits + 1
             endif
         endif
     enddo
-    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y
+    area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y ! Medida da parábola
 end subroutine parabola
 
 end module conicas
