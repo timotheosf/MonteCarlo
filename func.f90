fa35667f78e100810811c6875d7656b8bbdbcd89
timotheosf <timoteofassoni@gmail.com>
2024-11-16 06:55:16 -0300

    Comentários

--- func.f90
+++ func.f90
@@ -13,7 +13,9 @@ private x_r , y_r , num_hits , num_trials , seed , generator
 
 contains
 
-function f_( x , a , b , c , fig ) result( result ) 
+function f_( x , a , b , c , fig ) result( result )
+    ! Essa função seleciona de qual função será analisada 
+    ! a área debaixo de sua curva
     real(kind=dp) , intent(in) :: x , a , b , c
     real(kind=dp) :: result
     character(len=5) , intent(in) :: fig
@@ -29,6 +31,9 @@ function f_( x , a , b , c , fig ) result( result )
 end function f_
 
 subroutine area_under_f( a , b , c , fig , L_x , L_y , area )
+    ! Subrotina para cálculo da área entre o gráfico de uma função e o eixo y
+    ! Veja que o resultado NÃO É igual à integral da função,
+    ! mas, sim, igual à integral do móduo da função
     real(kind=dp), intent(in) :: a , b , c , L_x , L_y
     real(kind=dp), intent(out) :: area
     character(len=5) , intent(in) :: fig
@@ -36,8 +41,8 @@ subroutine area_under_f( a , b , c , fig , L_x , L_y , area )
     do i = 1 , num_trials
         x_r = generator%real( -0.5*L_x , 0.5*L_x ) ! Gera duplas de números ao longo de todo o sistema
         y_r = generator%real( -0.5*L_y , 0.5*L_y )
-        if ( (f_( x_r , a , b , c , fig ) <= y_r .and. y_r <= 0. ) .or. ( 0. <= y_r .and. y_r <= f_( x_r , a , b , c , fig )))& ! quebra de linha para o gfortran não reclamar
-         num_hits = num_hits + 1
+        if ( (f_( x_r , a , b , c , fig ) <= y_r .and. y_r <= 0. ) .or. ( 0. <= y_r .and. y_r <= f_( x_r , a , b , c , fig )))& ! Quebra de linha para o gfortran não reclamar
+         num_hits = num_hits + 1 ! Conta para as regiões em que f_ é negativa ou positiva
     enddo
     area = real(num_hits,kind=dp)/real(num_trials,kind=dp) * L_x * L_y 
 end subroutine area_under_f
