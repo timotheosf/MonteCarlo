b54c87e3670d369e9bdb18e118820b29024aa119
timotheosf <timoteofassoni@gmail.com>
2024-11-16 19:21:41 -0300

    Implementando as funções do novo módulo

--- area.f90
+++ area.f90
@@ -1,12 +1,14 @@
 program calculo_de_area_monte_carlo
 use conicas 
 use func
+use any_shape
 use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
 implicit none
 real(kind=dp) a , b , c , L_x , L_y , area
 character(len=5) figure
 character(len=100) arg
 character(len=1) first_char
+character(len=10) PATH
 
 call get_command_argument( 1 , arg ) ; read(arg,*) L_x ! Lê o tamanho do sistema em x
 call get_command_argument( 2 , arg ) ; read(arg,*) L_y ! Lê o tamanho do sistema em y
@@ -49,6 +51,11 @@ else if ( first_char=='f' ) then
     call get_command_argument( 4 , arg ) ; read(arg,*) c ! Lê o terceiro argumento
     call write_params( L_x , L_y , figure , a , b , c )
     call area_under_f( a , b , c , figure , L_x , L_y , area )
+    print*, area
+
+else if ( figure=='data_' ) then
+    call get_command_argument( 4 , PATH )
+    call any( L_x , L_y , PATH , area )
     print*, area
 endif
 
